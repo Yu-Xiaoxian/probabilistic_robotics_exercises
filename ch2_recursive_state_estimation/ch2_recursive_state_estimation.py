431c9f69a8a3a838ebd119936500b15cceaf7e5b
xiaoxian <self@yu-xiaoxian.me>
2020-04-06 02:28:23 +0800

    :sparkles: feat: solution for Ex2.2

--- ch2_recursive_state_estimation/ch2_recursive_state_estimation.py
+++ ch2_recursive_state_estimation/ch2_recursive_state_estimation.py
@@ -21,6 +21,7 @@
 # SOFTWARE.
 
 import numpy as np
+import random as rd
 
 print("Chapter 2: Recursive State Estimation")
 print("")
@@ -34,3 +35,29 @@ for i in range(1 , 11):
     eta = 1/(p[0] + p[1])
     p = [p[0]*eta, p[1]*eta]
     print(i, p[0], p[1], eta)
+
+print("")
+print("#############")
+print("Exercise 2.2:")
+cnt_weath = [0,0,0]
+weath = ["sunny", "cloudy", "windy"]
+cnt = 0
+trans = [[0.8, 1.0, 1.0],[0.4, 0.8,1.0],[0.2,0.8,1.0]]
+
+yest = 0
+today = 0
+while cnt < 10000000:
+	tran = trans[yest]
+	p = rd.random()
+	if p < tran[0]:
+		today = 0
+	elif p < tran[1]:
+		today = 1
+	else:
+		today = 2
+	# print(weath[today])
+	cnt_weath[today] = cnt_weath[today] + 1
+	cnt = cnt+1
+	yest = today
+
+print(float(cnt_weath[0])/cnt, float(cnt_weath[1])/cnt, float(cnt_weath[2])/cnt)
