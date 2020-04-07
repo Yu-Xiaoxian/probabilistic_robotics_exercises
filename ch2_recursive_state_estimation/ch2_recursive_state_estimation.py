ef02067153e78dbb438274279698a926c5e9bae9
xiaoxian <self@yu-xiaoxian.me>
2020-04-07 23:31:00 +0800

    :sparkles: feat: add solution for Ex2.3 [1/2]

--- ch2_recursive_state_estimation/ch2_recursive_state_estimation.py
+++ ch2_recursive_state_estimation/ch2_recursive_state_estimation.py
@@ -61,3 +61,35 @@ while cnt < 10000000:
 	yest = today
 
 print(float(cnt_weath[0])/cnt, float(cnt_weath[1])/cnt, float(cnt_weath[2])/cnt)
+
+print("")
+print("#############")
+print("Exercise 2.3: a)")
+trans = [[0.8, 0.4, 0.2],[0.2, 0.4, 0.6],[0.0,0.2,0.2]]
+obser = [[0.6, 0.3, 0.0],[0.4, 0.7, 0.0],[0.0,0.0,0.1]]
+
+bel = [1.0, 0, 0]
+z = [1, 1, 2, 0]
+
+for i in range(0,4):
+	bar_bel = [bel[0]*trans[0][0] + bel[1]*trans[0][1] + bel[2]*trans[0][2], bel[0]*trans[1][0] + bel[1]*trans[1][1] + bel[2]*trans[1][2], bel[0]*trans[2][0] + bel[1]*trans[2][1] + bel[2]*trans[2][2]]
+	bel = [obser[z[i]][0]*bar_bel[0], obser[z[i]][1]*bar_bel[1], obser[z[i]][2]*bar_bel[2]]
+	eta = 1/(bel[0] + bel[1] + bel[2])
+	bel = [bel[0]*eta, bel[1]*eta, bel[2]*eta]
+	print("Day ", i+2)
+	# print(bar_bel)
+	print(bel)
+	print(eta)
+
+print("b)")
+bel = [1.0, 0, 0]
+z = [0, 0, 2]
+for i in range(0,3):
+	bar_bel = [bel[0]*trans[0][0] + bel[1]*trans[0][1] + bel[2]*trans[0][2], bel[0]*trans[1][0] + bel[1]*trans[1][1] + bel[2]*trans[1][2], bel[0]*trans[2][0] + bel[1]*trans[2][1] + bel[2]*trans[2][2]]
+	bel = [obser[z[i]][0]*bar_bel[0], obser[z[i]][1]*bar_bel[1], obser[z[i]][2]*bar_bel[2]]
+	eta = 1/(bel[0] + bel[1] + bel[2])
+	bel = [bel[0]*eta, bel[1]*eta, bel[2]*eta]
+	print("Day ", i+2)
+	# print(bar_bel)
+	print(bel)
+	print(eta)
