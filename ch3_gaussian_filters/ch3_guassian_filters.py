423068be35c4818e0eb79f81ce571f1eedec7943
xiaoxian <self@yu-xiaoxian.me>
2020-04-14 23:25:19 +0800

    :sparkles: feat: add solution for Ex3.1

--- ch3_gaussian_filters/ch3_guassian_filters.py
+++ ch3_gaussian_filters/ch3_guassian_filters.py
@@ -20,7 +20,37 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
 
+import numpy as np
+from matplotlib.patches import Ellipse
+import matplotlib.pyplot as plt
+
 print("Chapter 3: Guassian Filters")
 print("")
 print("#############")
 print("Exercise 3.1:")
+
+A = np.mat([[1,1],[0,1]])
+B = np.mat([[0.5], [1.0]])
+R = B*B.T
+mu = np.mat([[0], [0]])
+u = np.mat([0])
+Sigma = np.mat([[0,0],[0,0]])
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+for i in range(1, 6):
+    mu_t = A*mu + B*u
+    Sigma_t = A*Sigma*A.T + R
+    mu = mu_t
+    Sigma = Sigma_t
+    print(mu)
+    print(Sigma)
+    eigen_value, eigen_vector = np.linalg.eig(Sigma)
+    ell1 = Ellipse(xy = (0.0, 0.0), width = eigen_value[1], height = eigen_value[0], angle = np.arctan(eigen_vector[1,0]/eigen_vector[0,0])*180.0/3.1415926, facecolor='yellow', alpha=0.3)
+    ax.add_patch(ell1)
+
+plt.axis('scaled')
+plt.axis('equal')   #changes limits of x or y axis so that equal increments of x and y have the same length
+
+plt.show()
